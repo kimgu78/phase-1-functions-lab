885629315363b51c4b4d43acfbea6b513cab4046
kimgu78 <kimgu78@gmail.com>
2022-02-05 23:04:06 -0700

    commit tree 9d204e04bf7b1d05d9154f7668b3fadeb1fbace8

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(aValue) {
+    const hq = 42;
+    if (aValue > 42) 
+    return (aValue - hq)
+    if (aValue < 42) 
+    return  (hq - aValue)
+}
+function distanceFromHqInFeet(aValue) {
+    const hq = 42;
+    if (aValue > 42)
+    return (aValue - hq) * 264
+    if (aValue < 42)
+    return (hq - aValue) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination)
+    return (start - destination) * 264
+    if (destination > start)
+    return (destination - start) * 264
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400)
+    return 0
+    if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <2000)
+    return (distanceTravelledInFeet(start, destination) - 400) * .02
+    if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500)
+    return 25
+    if (distanceTravelledInFeet(start, destination) > 2500)
+    return `cannot travel that far`
+}
